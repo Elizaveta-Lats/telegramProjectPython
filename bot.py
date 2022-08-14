e77e1eb2a2f3807acd0e0d1416f769be80c93765
Елизавета <l.latc@inbox.ru>
2022-08-15 02:13:33 +0300

    Update bot.py
    Создала кнопки клавиатуры и хоть какую-то реакцию на них прописала

--- bot.py
+++ bot.py
@@ -17,9 +17,11 @@ birthdays_and_names = {'01-06': ["Итто", "Паймон"], '24-07': ["Сик�
 @bot.message_handler(commands=['start', 'go'])
 def start_handler(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    btn1 = types.KeyboardButton("👋 Поздороваться")
-    btn2 = types.KeyboardButton("❓ Задать вопрос")
-    markup.add(btn1, btn2)
+    btn_hello = types.KeyboardButton("👋 Поздороваться")
+    btn_closest_bday = types.KeyboardButton("❓ Ближайший день рождения")
+    btn_bdays_that_month = types.KeyboardButton("🎉 Дни рождения в этом месяце")
+    btn_bdays_selected_month = types.KeyboardButton("🎂 Дни рождения в заданном месяце")
+    markup.add(btn_hello, btn_closest_bday, btn_bdays_that_month, btn_bdays_selected_month)
 
     bot.send_message(message.chat.id, 'Привет, когда я вырасту, я буду сообщать про ДР персонажей геншина',
                      reply_markup=markup)
@@ -34,8 +36,49 @@ def help_handler(message):
 
 
 @bot.message_handler(content_types=['text'])
+# потом, наверное, распихаю по отдельным хэндлерам и создам команды для всех этих функций
+# а мб заменю потом на inline кнопки, хотя бы часть из них
 def get_text_messages(message):
-    bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help")
+    if message.text == "👋 Поздороваться":
+        bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAEFj9pi-XwJ6DuPzfFzrNhgFAo74cNd6gACPw8AAnqS-EtUlG9v__OqzSkE')
+    elif message.text == "❓ Ближайший день рождения":
+        bot.send_message(message.chat.id, 'Эта функция в процессе разработки ⚙️')
+    elif message.text == "🎉 Дни рождения в этом месяце":
+        bot.send_message(message.chat.id, '⚙ Эта функция в процессе разработки ⚙️')
+    elif message.text == "🎂 Дни рождения в заданном месяце":
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn_jan = types.KeyboardButton("Январь")
+        btn_feb = types.KeyboardButton("Февраль")
+        btn_mar = types.KeyboardButton("Март")
+        btn_apr = types.KeyboardButton("Апрель")
+        btn_may = types.KeyboardButton("Май")
+        btn_jun = types.KeyboardButton("Июнь")
+        btn_jul = types.KeyboardButton("Июль")
+        btn_aug = types.KeyboardButton("Август")
+        btn_sep = types.KeyboardButton("Сентябрь")
+        btn_oct = types.KeyboardButton("Октябрь")
+        btn_nov = types.KeyboardButton("Ноябрь")
+        btn_dec = types.KeyboardButton("Декабрь")
+        btn_back = types.KeyboardButton("Вернуться в главное меню")
+        markup.add(btn_jan, btn_feb, btn_mar, btn_apr, btn_may, btn_jun, btn_jul, btn_aug, btn_sep,
+                   btn_oct, btn_nov, btn_dec, btn_back)
+        bot.send_message(message.chat.id, text="Какой месяц интересует?", reply_markup=markup)
+    elif message.text == "Январь" or message.text == "Февраль" or message.text == "Март" or message.text == "Апрель"\
+            or message.text == "Май" or message.text == "Июнь" or message.text == "Июль" or message.text == "Август"\
+            or message.text == "Сентябрь" or message.text == "Октябрь" or message.text == "Ноябрь"\
+            or message.text == "Декабрь":
+        bot.send_message(message.chat.id, 'Да, это тоже в процессе разработки ⚙️')
+    elif message.text == "Вернуться в главное меню":
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn_hello = types.KeyboardButton("👋 Поздороваться")
+        btn_closest_bday = types.KeyboardButton("❓ Ближайший день рождения")
+        btn_bdays_that_month = types.KeyboardButton("🎉 Дни рождения в этом месяце")
+        btn_bdays_selected_month = types.KeyboardButton("🎂 Дни рождения в заданном месяце")
+        markup.add(btn_hello, btn_closest_bday, btn_bdays_that_month, btn_bdays_selected_month)
+        bot.send_message(message.chat.id, text="Вы вернулись в главное меню", reply_markup=markup)
+
+    else:
+        bot.send_message(message.chat.id, "Я тебя не понимаю. Напиши /help")
 
 
 def send_birthday_msg(name):
