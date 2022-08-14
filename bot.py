fe736327bcc1de041ee59f5ba19851d4fa088a4c
Елизавета <l.latc@inbox.ru>
2022-08-15 00:14:36 +0300

    Update bot.py
    Улучшение алгоритма проверки даты и времени, добавление заготовки клавиатуры (в процессе разработки)

--- bot.py
+++ bot.py
@@ -2,33 +2,49 @@ from datetime import datetime, date, time
 from decouple import config
 from threading import Thread
 import telebot
+from telebot import types
 import time as timer
 
 # в 2:00 по МСК (в 0:00 по времени сервера Европы (GMT+1)) присылаются письма от персонажей
 TOKEN = config('token', default='')
 bot = telebot.TeleBot(TOKEN)
 
-time_of_msg = time(10)  # 10:00:00 по МСК, время публикации сообщений
+time_of_msg = time(0, 10)  # 00:10:00 по МСК, время публикации сообщений
 
-birthdays_and_greetings = [[datetime.combine(date(2022, 8, 14), time_of_msg),
-                            datetime.combine(date(2022, 8, 14), time(16, 48))
+birthdays_and_greetings = [[date(2022, 8, 13),
+                            date(2022, 8, 14),
+                            date(2022, 8, 15),
                             ],
-                           ["ДР 1", "ДР 2"]]
+                           [["Эмбер"], ["Сангономия Кокоми", "Кудзё Сара"], ["Ци Ци"]]
+                           ]
+
+
+@bot.message_handler(commands=['start', 'go'])
+def start_handler(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn1 = types.KeyboardButton("👋 Поздороваться")
+    btn2 = types.KeyboardButton("❓ Задать вопрос")
+    markup.add(btn1, btn2)
+
+    bot.send_message(message.chat.id, 'Привет, когда я вырасту, я буду сообщать про ДР персонажей геншина',
+                     reply_markup=markup)
+    bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAEFj6Zi-UrjcS6F5m3hXB44mFi_KPDn2wACdw0AAnDa8UsVqatk3kgIuSkE')
+
+
+@bot.message_handler(commands=['help'])
+def help_handler(message):
+    bot.send_message(message.chat.id, "В этом канале https://t.me/birthdaysGenshin "
+                                      "я буду тебе напоминать про дни рождения "
+                                      "персонажей Genshin Impact. Переходи туда!")
 
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-    if message.text == "/help":
-        bot.send_message(message.from_user.id, "Привет, в этом канале https://t.me/birthdaysGenshin "
-                                               "я буду тебе напоминать про дни рождения "
-                                               "персонажей Genshin Impact. Переходи туда!")
-    else:
-        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help")
+    bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help")
 
 
-def send_birthday_msg(i):
-    greeting = birthdays_and_greetings[1][i]
-    bot.send_message("@birthdaysGenshin", str(greeting))
+def send_birthday_msg(name):
+    bot.send_message("@birthdaysGenshin", "{name} сегодня празднует свой день рождения!".format(name=name))
     # когда будет пак картинок для сообщений, надо заменить send_message на send_photo
     # bot.send_photo("@birthdaysGenshin", photo, caption='желаемый текст')
 
@@ -37,14 +53,33 @@ birthdays = birthdays_and_greetings[0]
 
 
 def check_time():
+    """
+    Метод постоянно прокручивает массив дней рождений.
+    Когда находит совпадение по дате, проверяет время.
+    Если оно больше или равно заданного в переменной и сегодня он еще не публиковал ничего,
+    тогда метод публикует сообщение на канале и делает отметку, что он это сделал.
+    Отметка сбрасывается при наступлении следующего дня
+    """
+    day1 = ''
+    is_msg_sent = False
     while True:
+        today = str(date.today())[5:]
+        if day1 != today:
+            day1 = today
+            is_msg_sent = False
+
         for birthday in birthdays:
-            bday = str(birthday)[5:16]
-            now = str(datetime.now())[5:16]
-            if bday == now:
-                index = birthdays.index(birthday)
-                send_birthday_msg(index)
-                timer.sleep(60)  # сон на 1 минуту
+            bday = str(birthday)[5:]
+            if bday == today:
+                time_now = datetime.now().time()
+                if time_now >= time_of_msg and not is_msg_sent:
+                    index = birthdays.index(birthday)
+                    names = birthdays_and_greetings[1][index]
+                    for name in names:
+                        send_birthday_msg(name)
+                        timer.sleep(15)
+                    is_msg_sent = True
+        timer.sleep(600)  # сон на 10 минут
 
 
 thread_time = Thread(target=check_time)  # поток для проверки времени
