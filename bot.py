e00571eb17a71fa5f332edd66e5c0b63068e317b
Елизавета <l.latc@inbox.ru>
2022-08-15 18:19:49 +0300

    Update bot.py
    Оптимизация кода

--- bot.py
+++ bot.py
@@ -7,84 +7,157 @@ import time as timer
 
 # в 2:00 по МСК (в 0:00 по времени сервера Европы (GMT+1)) присылаются письма от персонажей
 TOKEN = config('token', default='')
-bot = telebot.TeleBot(TOKEN)
+bot = telebot.TeleBot(TOKEN, parse_mode='HTML')
 
-time_of_msg = time(0, 10)  # 00:10:00 по МСК, время, после которого нужно публиковать сообщения
+time_of_msg = time(23, 10)  # 23:10:00 по МСК, время, после которого нужно публиковать сообщения
 
 birthdays_and_names = {'01.06': ["Итто", "Паймон"], '24.07': ["Сиканоин Хэйдзо"], '15.08': ["Коллеи"]}
+
+text_for_btn_hello = "👋 Поздороваться"
+text_for_btn_closest_bday = "❓ Ближайший ДР"
+text_for_btn_that_month_bdays = "🎉 Дни рождения в этом месяце"
+text_for_btn_selected_month_bdays = "🎂 Дни рождения в заданном месяце"
+text_for_btn_bday_closest_to_my_bday = "🤔 Узнать, чей ДР ближе всего к моему"
+text_for_btn_find_bday_of_char = "🔍 Узнать ДР персонажа"
 
 
 @bot.message_handler(commands=['start', 'go'])
 def start_handler(message):
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    btn_hello = types.KeyboardButton("👋 Поздороваться")
-    btn_closest_bday = types.KeyboardButton("❓ Ближайший день рождения")
-    btn_bdays_that_month = types.KeyboardButton("🎉 Дни рождения в этом месяце")
-    btn_bdays_selected_month = types.KeyboardButton("🎂 Дни рождения в заданном месяце")
-    markup.add(btn_hello, btn_closest_bday, btn_bdays_that_month, btn_bdays_selected_month)
-
     bot.send_message(message.chat.id, 'Привет, когда я вырасту, я буду сообщать про ДР персонажей геншина',
-                     reply_markup=markup)
+                     reply_markup=main_menu())
     bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAEFj6Zi-UrjcS6F5m3hXB44mFi_KPDn2wACdw0AAnDa8UsVqatk3kgIuSkE')
 
 
-@bot.message_handler(commands=['help'])
-def help_handler(message):
-    bot.send_message(message.chat.id, "В этом канале https://t.me/birthdaysGenshin "
+@bot.message_handler(commands=['help', 'about'])
+def about_handler(message):
+    bot.send_message(message.chat.id, "Привет!\nЗдесь ты можешь узнать, чей день рождения скоро наступит, "
+                                      "у кого дни рождения в текущем (или выбранном тобой) месяце. "
+                                      "Для этого тебе нужно всего лишь нажать на кнопки внизу!\n\n"
+                                      "Кроме того, в этом канале https://t.me/birthdaysGenshin "
                                       "я буду тебе напоминать про дни рождения "
                                       "персонажей Genshin Impact. Переходи туда!")
 
 
+@bot.message_handler(commands=['hello'])
+def hello_handler(message):
+    send_hello_msg(message)
+
+
+@bot.message_handler(commands=['closestbday'])
+def closest_bday_handler(message):
+    send_closest_bday(message)
+
+
+@bot.message_handler(commands=['bdaysthatmonth'])
+def bdays_that_month_handler(message):
+    send_that_month_bdays(message)
+
+
+@bot.message_handler(commands=['bdaysselectedmonth'])
+def bdays_selected_month_handler(message):
+    send_selected_month_bdays(message)
+
+
+@bot.message_handler(commands=['bdayclosesttomybday'])
+def bdays_closest_to_my_bday_handler(message):
+    send_bday_closest_to_my_bday(message)
+
+
+@bot.message_handler(commands=['findbdayofchar'])
+def find_bday_of_char_handler(message):
+    send_bday_of_char(message)
+
+
 @bot.message_handler(content_types=['text'])
-# потом, наверное, распихаю по отдельным хэндлерам и создам команды для всех этих функций
-# а мб заменю потом на inline кнопки, хотя бы часть из них
+# заменить на inline кнопки выбор месяца (с inline пока не получилось)
 def get_text_messages(message):
-    if message.text == "👋 Поздороваться":
-        bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAEFj9pi-XwJ6DuPzfFzrNhgFAo74cNd6gACPw8AAnqS-EtUlG9v__OqzSkE')
-    elif message.text == "❓ Ближайший день рождения":
-        bot.send_message(message.chat.id, 'Эта функция в процессе разработки ⚙️')
-    elif message.text == "🎉 Дни рождения в этом месяце":
-        bot.send_message(message.chat.id, '⚙ Эта функция в процессе разработки ⚙️')
-    elif message.text == "🎂 Дни рождения в заданном месяце":
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        btn_jan = types.KeyboardButton("Январь")
-        btn_feb = types.KeyboardButton("Февраль")
-        btn_mar = types.KeyboardButton("Март")
-        btn_apr = types.KeyboardButton("Апрель")
-        btn_may = types.KeyboardButton("Май")
-        btn_jun = types.KeyboardButton("Июнь")
-        btn_jul = types.KeyboardButton("Июль")
-        btn_aug = types.KeyboardButton("Август")
-        btn_sep = types.KeyboardButton("Сентябрь")
-        btn_oct = types.KeyboardButton("Октябрь")
-        btn_nov = types.KeyboardButton("Ноябрь")
-        btn_dec = types.KeyboardButton("Декабрь")
-        btn_back = types.KeyboardButton("Вернуться в главное меню")
-        markup.add(btn_jan, btn_feb, btn_mar, btn_apr, btn_may, btn_jun, btn_jul, btn_aug, btn_sep,
-                   btn_oct, btn_nov, btn_dec, btn_back)
-        bot.send_message(message.chat.id, text="Какой месяц интересует?", reply_markup=markup)
-    elif message.text == "Январь" or message.text == "Февраль" or message.text == "Март" or message.text == "Апрель"\
-            or message.text == "Май" or message.text == "Июнь" or message.text == "Июль" or message.text == "Август"\
-            or message.text == "Сентябрь" or message.text == "Октябрь" or message.text == "Ноябрь"\
+    if message.text == text_for_btn_hello or message.text.lower() == "hello" or message.text.lower() == "привет":
+        send_hello_msg(message)
+    elif message.text == text_for_btn_closest_bday:
+        send_closest_bday(message)
+    elif message.text == text_for_btn_that_month_bdays:
+        send_that_month_bdays(message)
+    elif message.text == text_for_btn_selected_month_bdays:
+        send_selected_month_bdays(message)
+    elif message.text == "Январь" or message.text == "Февраль" or message.text == "Март" or message.text == "Апрель" \
+            or message.text == "Май" or message.text == "Июнь" or message.text == "Июль" or message.text == "Август" \
+            or message.text == "Сентябрь" or message.text == "Октябрь" or message.text == "Ноябрь" \
             or message.text == "Декабрь":
         bot.send_message(message.chat.id, 'Да, это тоже в процессе разработки ⚙️')
+    elif message.text == text_for_btn_bday_closest_to_my_bday:
+        send_bday_closest_to_my_bday(message)
+    elif message.text == text_for_btn_find_bday_of_char:
+        send_bday_of_char(message)
     elif message.text == "Вернуться в главное меню":
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        btn_hello = types.KeyboardButton("👋 Поздороваться")
-        btn_closest_bday = types.KeyboardButton("❓ Ближайший день рождения")
-        btn_bdays_that_month = types.KeyboardButton("🎉 Дни рождения в этом месяце")
-        btn_bdays_selected_month = types.KeyboardButton("🎂 Дни рождения в заданном месяце")
-        markup.add(btn_hello, btn_closest_bday, btn_bdays_that_month, btn_bdays_selected_month)
-        bot.send_message(message.chat.id, text="Вы вернулись в главное меню", reply_markup=markup)
+        bot.send_message(message.chat.id, text="Вы вернулись в главное меню", reply_markup=main_menu())
 
     else:
-        bot.send_message(message.chat.id, "Я тебя не понимаю. Напиши /help")
+        bot.send_message(message.chat.id, "Я тебя не понимаю. Напиши /help или /about")
 
 
 def send_birthday_msg(name):
-    bot.send_message("@birthdaysGenshin", "{name} сегодня празднует свой день рождения!".format(name=name))
+    bot.send_message("@birthdaysGenshin",
+                     "<b>{name}</b> сегодня празднует свой день рождения!".format(name=name))
     # когда будет пак картинок для сообщений, надо заменить send_message на send_photo
     # bot.send_photo("@birthdaysGenshin", photo, caption='желаемый текст')
+
+
+def send_hello_msg(message):
+    bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAEFj9pi-XwJ6DuPzfFzrNhgFAo74cNd6gACPw8AAnqS-EtUlG9v__OqzSkE')
+
+
+def send_closest_bday(message):
+    bot.send_message(message.chat.id, 'Эта функция в процессе разработки ⚙️')
+
+
+def send_that_month_bdays(message):
+    bot.send_message(message.chat.id, '⚙ Эта функция в процессе разработки ⚙️')
+
+
+def send_selected_month_bdays(message):
+    bot.send_message(message.chat.id, text="Какой месяц интересует?", reply_markup=month_menu())
+
+
+def send_bday_closest_to_my_bday(message):
+    bot.send_message(message.chat.id, '⚙ Эта функция очень нескоро будет готова 😢️')
+
+
+def send_bday_of_char(message):
+    bot.send_message(message.chat.id, 'Я бы с радостью рассказал, но пока не умею 😢️')
+
+
+def main_menu():
+    main_keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    main_keyboard.row_width = 2
+    btn_hello = types.KeyboardButton(text_for_btn_hello)
+    btn_closest_bday = types.KeyboardButton(text_for_btn_closest_bday)
+    btn_bdays_that_month = types.KeyboardButton(text_for_btn_that_month_bdays)
+    btn_bdays_selected_month = types.KeyboardButton(text_for_btn_selected_month_bdays)
+    btn_bday_closest_to_my_bday = types.KeyboardButton(text_for_btn_bday_closest_to_my_bday)
+    btn_find_bday_of_char = types.KeyboardButton(text_for_btn_find_bday_of_char)
+    main_keyboard.add(btn_hello, btn_closest_bday, btn_bdays_that_month, btn_bdays_selected_month,
+                      btn_bday_closest_to_my_bday, btn_find_bday_of_char)
+    return main_keyboard
+
+
+def month_menu():
+    month_keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn_jan = types.KeyboardButton("Январь")
+    btn_feb = types.KeyboardButton("Февраль")
+    btn_mar = types.KeyboardButton("Март")
+    btn_apr = types.KeyboardButton("Апрель")
+    btn_may = types.KeyboardButton("Май")
+    btn_jun = types.KeyboardButton("Июнь")
+    btn_jul = types.KeyboardButton("Июль")
+    btn_aug = types.KeyboardButton("Август")
+    btn_sep = types.KeyboardButton("Сентябрь")
+    btn_oct = types.KeyboardButton("Октябрь")
+    btn_nov = types.KeyboardButton("Ноябрь")
+    btn_dec = types.KeyboardButton("Декабрь")
+    btn_back = types.KeyboardButton("Вернуться в главное меню")
+    month_keyboard.add(btn_jan, btn_feb, btn_mar, btn_apr, btn_may, btn_jun, btn_jul, btn_aug, btn_sep,
+                       btn_oct, btn_nov, btn_dec, btn_back)
+    return month_keyboard
 
 
 def check_time():
