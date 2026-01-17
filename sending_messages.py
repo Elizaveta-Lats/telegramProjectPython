c9bbdb8093a2dd848bfbfe04c9c1716cd1d8ee1a
Elizaveta-Lats <l.latc@inbox.ru>
2026-01-17 14:53:58 +0300

    Исправление проверки високосного года.

--- sending_messages.py
+++ sending_messages.py
@@ -1,5 +1,6 @@
 import os
 from datetime import date
+import calendar
 import telebot
 import sqlite3
 import time as timer
@@ -21,7 +22,7 @@ def send_birthday_msg(name):
 
 def handler(event, _):
     today = date.today()
-    if int(today.day) == 28 and int(today.month) == 2 and int(today.year) % 4 != 0:
+    if int(today.day) == 28 and int(today.month) == 2 and calendar.isleap(int(today.year)):
         cursor.execute('SELECT name FROM birthdays WHERE bday = "2020-02-28" OR bday = "2020-02-29"')
     else:
         cursor.execute(f'SELECT name FROM birthdays WHERE bday = "{str(date(2020, int(today.month), int(today.day)))}"')
